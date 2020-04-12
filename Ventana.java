eb1f5db6d1e22ddd04e1254d86356f8d080071eb
Andres Lederer <dobrandres@gmail.com>
2020-04-12 16:51:38 -0300

    Agrego props a < JLabel etiquetaTxt1 >

--- Ventana.java
+++ Ventana.java
@@ -1,7 +1,9 @@
 package Ventana;
 
 //librerias importadas
-import java.awt.Color;
+import java.awt.Color; //Lib para colores de bg y fg
+import java.awt.Font; //Lib para definir fuente de JLabel de texto
+
 import javax.swing.JFrame; //Lib para ventana emergente
 import javax.swing.JPanel; //Lib para panel en ventana
 import javax.swing.SwingConstants;
@@ -42,18 +44,20 @@ public class Ventana extends JFrame {
 		
 		
 		
-		JLabel etiquetaTxt1 = new JLabel(); //creo etiqueta de txt
-		etiquetaTxt1.setText("My best memories in pictures!"); //defino el txt de la etiqueta
-		etiquetaTxt1.setBounds(10,10,200,30); //reposiciono etiquetaTxt1
-		etiquetaTxt1.setHorizontalAlignment(SwingConstants.CENTER); //defino la posicion del txt EN la ETIQUETA
+		//JLabel etiquetaTxt1 = new JLabel(); //creo etiqueta de txt
+		JLabel etiquetaTxt1 = new JLabel("My best memories in pictures",SwingConstants.CENTER);
+		//etiquetaTxt1.setText("My best memories in pictures!"); //defino el txt de la etiqueta
+		//etiquetaTxt1.setHorizontalAlignment(SwingConstants.CENTER); //defino la posicion del txt EN la ETIQUETA
+		etiquetaTxt1.setBounds(10,10,400,30); //reposiciono etiquetaTxt1
 		
+		etiquetaTxt1.setFont(new Font ("arial",Font.BOLD,20));//defino fuente,estilo y tamaño del texto
 		etiquetaTxt1.setForeground(Color.WHITE); //defino el color de las letras del texto
 		etiquetaTxt1.setOpaque(true); //desactivo el layout x defecto de las etiquetas => puedo definir un bground color
 		etiquetaTxt1.setBackground(Color.BLUE);//defino el color de fondo de la JLlabel
 		
+		
+		
 		panel.add(etiquetaTxt1); //agrego la etiqueta al panel para q sea visible
-		
-		
 		
 		//FINALLY, AGREGO EL PANEL EN LA VENTANA EMERGENTE
 		this.getContentPane().add(panel); 
